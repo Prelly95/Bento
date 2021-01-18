55ec3a6d690eeb317903a14d9883099e94305a92
Patrick Prell <prelly95@gmail.com>
2021-01-19 09:37:57 +1100

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'ad0ca2c067fd4cfaae8fe5ff8e474145';
 
 // Set Position function
 setPosition();
